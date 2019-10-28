05a92eb6e81d494f33b5a3300644265ff8e32fd8
konstantin-boss <konstantin.boss7@gmail.com>
2019-10-28 11:38:17 +0100

    Add files via upload

---
+++ Homework 5/README.txt
@@ -0,0 +1,2 @@
+I include two Python scripts, where one is to solve for the mean of lnz (moments) and one is to solve the exercsises. 
+Output is given in the code as well as in the PDF. I use a simple Lagrange exercise to calculate optimal capital in exercise 1.
---
+++ Homework 5/moments.py
@@ -0,0 +1,41 @@
+import numpy as np
+import random
+from scipy.optimize import fsolve
+import math
+import scipy.integrate as integrate
+import scipy.special as special
+
+gamma = 0.8
+np.random.seed(13)
+
+### Checking the mean of ln(k) which I calculate as -0.5. Does this give E[k] =1?
+lnk = np.random.normal(-0.5,1,10000000)
+ek = np.exp(lnk)
+q = np.mean(ek)
+print(q) # If this is equal or close to 1 we are good
+
+
+
+### Finding the mean of ln(z). Does this give E[s] = 1?
+def integrand(s,mu):
+    return (1-gamma)/(np.sqrt(2*math.pi))*np.exp(-0.5*((1-gamma)*np.log(s)-mu)**2)
+
+def expints(mu):
+    return integrate.quad(integrand, 0, np.inf, args=(mu))[0]
+
+def int(mu):
+    f1 = 1-expints(mu)
+    return(f1)
+
+eq =fsolve(int,-1)
+print(eq) # This should be the mean that gives us E[s] = 1
+
+test = expints(eq)
+print(test) # Evaluating the integral for this mu should give us 1 as we have log-normal density
+
+
+lnz = np.random.normal(eq,1,1000000)
+z = np.exp(lnz)
+s = pow(z, 1/(1-gamma))
+m = np.mean(s)
+print(m) # This should be close to 1
